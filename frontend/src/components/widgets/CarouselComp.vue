729e19a84adabd7c9efb13ae010a85237d415bd4
sgnm-haydar <haydar.urdogan@signumtte.com>
2024-04-10 20:55:39 +0300

    home page carousel added

---
+++ frontend/src/components/widgets/CarouselComp.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="position-relative w-100">
+    <Transition name="fade" mode="out-in">
+      <div :key="selectedItem.imageUrl" class="position-relative">
+        <img :src="selectedItem.imageUrl" class="w-100" :style="{ height:height, objectfix: 'cover', }" />
+        <div
+          class="custom-carousel-caption position-absolute top-0 start-0 bottom-0 end-0 d-flex justify-content-center text-light p-4 align-items-center"
+        >
+          <div class="container">
+            <h6 class="fw-bold text-uppercase" style="color: var(--secondary-color)">
+              {{ selectedItem.subtitle }}
+            </h6>
+            <h1 class="display-4">{{ selectedItem.title }}</h1>
+            <p class="lead">{{ selectedItem.description }}</p>
+          </div>
+        </div>
+      </div>
+    </Transition>
+
+    <div class="custom-carousel-controls position-absolute d-flex gap-1">
+      <button
+        @click="prevItem"
+        class="border-0 rounded-circle d-flex justify-content-center align-items-center"
+      >
+        <span class="carousel-control-prev-icon"></span>
+      </button>
+      <button
+        @click="nextItem"
+        class="border-0 rounded-circle d-flex justify-content-center align-items-center"
+      >
+        <span class="carousel-control-next-icon"></span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CarouselComp',
+  props: {
+    items: {
+      type: Array,
+      required: true
+    },
+    height: {
+      type: String,
+      default: '100%'
+    }
+  },
+  data() {
+    return {
+      currentItemIndex: 0,
+      selectedItem: null
+    }
+  },
+  methods: {
+    nextItem() {
+      this.currentItemIndex = (this.currentItemIndex + 1) % this.items.length
+      this.selectedItem = this.items[this.currentItemIndex]
+    },
+    prevItem() {
+      this.currentItemIndex = (this.currentItemIndex - 1 + this.items.length) % this.items.length
+      this.selectedItem = this.items[this.currentItemIndex]
+    }
+  },
+  created() {
+    this.selectedItem = this.items[0]
+  }
+}
+</script>
+
+<style scoped>
+.custom-carousel-caption {
+  background-color: rgba(0, 0, 0, 0.7);
+}
+.custom-carousel-controls {
+  bottom: -22.5px;
+}
+.custom-carousel-controls button {
+  background-color: var(--secondary-color);
+  width: 50px;
+  height: 50px;
+  transition: all 0.3s ease;
+}
+
+.custom-carousel-controls button:hover {
+  background-color: var(--primary-color);
+  transform: scale(1.1);
+}
+.carousel-control-prev-icon,
+.carousel-control-next-icon {
+  width: 1rem;
+  height: 1rem;
+}
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-to {
+  opacity: 1;
+}
+
+.fade-enter-active {
+  transition: all 1s ease;
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,15 +1,47 @@
 <template>
-    <div>
-        HomeView Page
-    </div>
+  <section>
+    <CarouselComp  :items="carouselItems" :height="'400px'"/>
+  </section>
 </template>
 
 <script>
-    export default {
-        name:"HomeView"
+import CarouselComp from '@/components/widgets/CarouselComp.vue'
+import hero_1 from '@/assets/images/hero_1.jpg'
+import hero_2 from '@/assets/images/hero_2.jpg'
+import hero_3 from '@/assets/images/hero_3.jpg'
+export default {
+  name: 'HomeView',
+  components: {
+    CarouselComp
+  },
+  data() {
+    return {
+      carouselItems: [
+        {
+          imageUrl: hero_1,
+          subtitle: 'Liberte',
+          title: 'Lorem Ipsum Dolor Sit Amet',
+          description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        },
+        {
+          imageUrl: hero_2,
+          subtitle: 'Egalite',
+          title: 'Excepteur Sint Occaecat Cupidatat',
+          description:
+            'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        },
+        {
+          imageUrl: hero_3,
+          subtitle: 'Fraternite',
+          title: 'Neque Porro Quisquam Est',
+          description:
+            'Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.'
+        }
+      ]
     }
+  }
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
